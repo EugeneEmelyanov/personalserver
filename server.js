8b71a61a7d060a3de2704d93de2ede2f24011121
eugene <eugene@eugenes-MacBook-Air.local>
2015-08-26 14:35:02 -0700

    CORS fix

--- server.js
+++ server.js
@@ -93,6 +93,7 @@ var SampleApp = function() {
         self.app.use(function(req, res, next) {
             res.header("Access-Control-Allow-Origin", "*");
             res.header("Access-Control-Allow-Headers", "X-Requested-With");
+            res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
             next();
         });
     }
